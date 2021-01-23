8882bd644564681bd296491e8ec3a46bf09a177d
Votikk <wojciech.progr@gmail.com>
2021-01-23 15:12:38 +0100

    front i zalążek nowego pluginu

---
+++ wp-content/plugins/vot-opinions/vot-opinions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Vot Opinions
+* Plugin URI: 
+* Description: Plugin opinii Wordpress
+* Version: 1.0
+* Author: Wojciech Wielgosz
+* Author URI: **/
+?>
+Tekst mojej wtyczki
